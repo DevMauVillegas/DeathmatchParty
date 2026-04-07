685fd4bd48dca008daae0f4ce2897b34edd133a5
Martin Villegas Garcia <dev.mau.villegas@gmail.com>
2026-04-07 16:00:13 +0800

    Updating project file and fixing damage notifier for AI character

--- Source/DeathmatchParty/Characters/AIPartyCharacter.cpp
+++ Source/DeathmatchParty/Characters/AIPartyCharacter.cpp
@@ -2,21 +2,21 @@
 
 
 #include "AIPartyCharacter.h"
-//#include "Perception/AISense_Damage.h"
+#include "Runtime/AIModule/Classes/Perception/AISense_Damage.h"
 
 void AAIPartyCharacter::ReceiveDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType,
                                       AController* InstigatorController, AActor* DamageCauser)
 {
 	if (HasAuthority())
 	{
-		/*		UAISense_Damage::ReportDamageEvent(
-					GetWorld(),
-					this,
-					InstigatorController,
-					Damage,
-					DamageCauser->GetActorLocation(),
-					DamageCauser->GetActorLocation()
-				);*/
+		UAISense_Damage::ReportDamageEvent(
+			GetWorld(),
+			this,
+			InstigatorController,
+			Damage,
+			DamageCauser->GetActorLocation(),
+			DamageCauser->GetActorLocation()
+		);
 	}
 	
 	Super::ReceiveDamage(DamagedActor, Damage, DamageType, InstigatorController, DamageCauser);
